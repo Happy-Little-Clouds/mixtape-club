312576cd34ddd7d567463ef43a09e455e80f9e25
Joanna Tomassoni <jctomassoni@gmail.com>
2020-02-11 14:09:18 -0600

    build out rough Chat component. functionality there, needs styling.

---
+++ src/components/Chat.jsx
@@ -0,0 +1,90 @@
+import React, { Component, useContext } from "react";
+import io from "socket.io-client";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+class Chat extends Component{
+  constructor(props){
+      super(props);
+
+      this.state = {
+          username: '',
+          message: '',
+          messages: []
+      };
+
+      this.socket = io.connect('http://localhost:8000');
+
+      this.socket.on('RECEIVE_MESSAGE', (data) => {
+          this.addMessage(data);
+      });
+
+      this.addMessage = this.addMessage.bind(this);
+      this.sendMessage = this.sendMessage.bind(this);
+  }
+
+  addMessage(data) {
+    console.log(data);
+    this.setState({messages: [...this.state.messages, data]});
+    console.log(this.state.messages);
+  }
+
+  
+  sendMessage(ev) {
+    ev.preventDefault();
+    this.socket.emit('SEND_MESSAGE', {
+        author: this.state.username,
+        message: this.state.message
+    })
+    this.setState({message: ''});
+  }
+
+  render(){
+    console.log(this.state.messages);
+      return (
+          <div className="container" style={{margin: 200}}>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+              <div className="row">
+                  <div className="col-4">
+                      <div className="card">
+                          <div className="card-body">
+                              <div className="card-title">Global Chat</div>
+                              <hr/>
+                              <div className="messages">
+                                  {this.state.messages.map(message => {
+                                      return (
+                                          <div>
+                                            {message.author}: {message.message}
+                                          </div>
+                                      )
+                                  })}
+                              </div>
+
+                          </div>
+                          <div className="card-footer">
+                              <input type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} className="form-control"/>
+                              <br/>
+                              <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
+                              <br/>
+                              <button onClick={this.sendMessage} className="btn btn-primary form-control">Send</button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      );
+  }
+}
+
+
+export default Chat
